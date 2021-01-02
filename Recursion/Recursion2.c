3c8b9ebd94b33bf66081f9aaddbded84903f0d29
unknown <sharmatarun537@gmail.com>
2021-01-03 01:38:28 +0530

    Second Program

---
+++ Recursion/Recursion2.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+//Program to demonstrate static variable in Recursion
+
+int StaticRecursion(int n)
+{
+    static int x = 0;
+
+    if(n>0)
+    {
+        x++;
+        return StaticRecursion(n-1) + x;
+    }
+
+    return 0;
+}
+
+
+//Main for calling both the functions
+int main()
+{
+    int m = 6;
+    printf("%d",StaticRecursion(m));
+    return 0;
+}
+
