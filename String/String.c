e4233f195abae946aaf43e476d35a611454ff7d8
unknown <sharmatarun537@gmail.com>
2021-02-14 23:29:17 +0530

    Added uppercase & lowercase function

--- String/String.c
+++ String/String.c
@@ -2,21 +2,47 @@
 #include<stdio.h>
 
 //Function to find the length of the string
-int string_length(char *string)
+int string_length(char *String)
 {
     int i = 0;
     //traversing until null-character is found
-    while(string[i] != '\0')
+    while(String[i] != '\0')
         i++;
     //Returning i value
     return i;
 }
 
+//Uppercase or Lowercase
+void change_case(char *String, char type)
+{
+    if(type = 'U')
+    {
+        for(int i = 0;String[i] != '\0'; i++)
+        {
+            if(String[i] >= 97 && String[i] <= 122)
+            {
+                String[i] -= 32;
+            }
+        }
+    }
+
+    else
+    {
+        for(int i = 0;String[i] != '\0'; i++)
+        {
+            if(String[i] >= 65 && String[i] <= 90)
+            {
+                String[i] += 32;
+            }
+        }
+    }
+}
 
 //Driver Function
 int main()
 {
     char *s = "GitHub";
-    printf("%d",string_length(s));
+    change_case(s,'U');
+    printf("%s",s);
     return 0;
 }
